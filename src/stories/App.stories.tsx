ad77b69d9b644a949f1d87d657c4b9581de7ef3e
Luke Robertson <luke.r@canva.com>
2021-05-15 11:29:25 +1000

    .

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -2,8 +2,5 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import App from '../App';
 
-// storiesOf('app', module).addWithVisreg('Cross Platform Promotion', () => {
-//   return <App />;
-// });
-
-export const app = () => <App />
+storiesOf('App', module)
+  .add('main app', () => <App />)
