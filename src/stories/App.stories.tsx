5775e2fd71dadc1428f068952651c521526e7975
Luke Robertson <luke.r@canva.com>
2021-05-15 11:25:13 +1000

    .

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,9 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import App from '../App';
+
+// storiesOf('app', module).addWithVisreg('Cross Platform Promotion', () => {
+//   return <App />;
+// });
+
+export const app = () => <App />
